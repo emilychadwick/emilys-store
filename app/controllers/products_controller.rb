2cf0556872d235b04b4d415e44f4f6525225b39d
Emily Chadwick <ekaseberg@gmail.com>
2017-03-05 14:44:16 -0800

    CF Lesson 5.3 WIP

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,14 @@ class ProductsController < ApplicationController
     end
   end
 
+  def self.search(search_term)
+    if Rails.env.development?
+      Product.where("name LIKE ?", "%#{search_term}")
+    else Rails.env.production?
+      Product.where("name ilike ?", "%#{search_term}")
+    end
+  end
+
 
   # GET /products/1
   # GET /products/1.json

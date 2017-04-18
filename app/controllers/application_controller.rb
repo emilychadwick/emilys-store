62b8b9977629a2d7ebfcc77f71c4157ffa3fd0bb
Emily Chadwick <ekaseberg@gmail.com>
2017-04-17 20:25:26 -0700

    CF Lesson 5.10 fixing glyphicon stars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email) }
+  end
+
+
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:user_id, :body, :rating)
+      params.require(:comment).permit(:user_id, :body, :rating, :first_name)
     end
 
 end

91a0dbce5f4c6518d1b175fead452d568650a791
Emily Chadwick <ekaseberg@gmail.com>
2017-06-26 20:33:36 -0700

    CF Lesson 5.11

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,10 @@ class UserMailer < ApplicationMailer
       :subject => "A new contact form message from #{name}")
   end
 
+  def welcome(user)
+    @appname = "Emily's Store"
+    mail(to: user.email,
+      subject: "Welcome to #{@appname}!")
+  end
+
 end

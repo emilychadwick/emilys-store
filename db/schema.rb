dfe17a845330bf0bb8e996d793688a6ffe1c1ae5
Emily Chadwick <ekaseberg@gmail.com>
2017-07-07 20:53:01 -0700

    CF Lesson 6.5 db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708032422) do
+ActiveRecord::Schema.define(version: 20170708035054) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20170708032422) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.integer  "price"
+    t.decimal  "price"
   end
 
   create_table "users", force: :cascade do |t|

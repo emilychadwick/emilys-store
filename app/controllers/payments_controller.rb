6e75b1d10ec6a2c1ffec2738c3bb91b1c4e149aa
Emily Chadwick <ekaseberg@gmail.com>
2017-07-08 12:48:08 -0700

    CF Lesson 6.6

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,42 @@
+class PaymentsController < ApplicationController
+
+  def create
+
+    @product = Product.find(param[:product_id])
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to new_user_registration_page
+    end
+
+    token = params[:stripeToken]
+    # create the charge on Stripe's servers to charge user card
+    begin
+      charge = Stripe::Charge.create(
+        amount: (@product.price*100).to_i, #amount in cents
+        currency: "usd",
+        source: token,
+        description: params[:stripeEmail]
+        receipt_email: "example@fake.com"
+      )
+
+      if charge.paid
+        Order.create(
+          product_id: @product.id,
+          user_id: @user.id,
+          total: @product.price
+          )
+        flash[:success] = "Your payment was processed successfully"
+      end
+
+    rescue Stripe::CardError => e
+      # the card has been declined
+      body = e.jason_body
+      err = body[:error]
+      flash[:error] = "Unforunately, there was an error processing your payment: #{err[:message]}"
+    end
+    redirect_to product_path(@product)
+    end
+  end
+
+end

bb62632efb0ca0ba7b408e768f7b001722169e3a
Emily Chadwick <ekaseberg@gmail.com>
2017-07-13 21:34:24 -0700

    6.9 Heroku tweaks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
 
   #compile assets
   config.assets.compile = true
+  config.serve_static_assets = true
   config.assets.digest = true
 
   # Eager load code on boot. This eager loads most of Rails and

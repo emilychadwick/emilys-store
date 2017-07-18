c7edbea093d708692f7bdfb210766f7e9576bc78
Emily Chadwick <ekaseberg@gmail.com>
2017-07-17 18:33:30 -0700

    CF Lesson 6.10 Last Time?

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,7 +34,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC")
-    @comments = @product.comments.paginate(:page => params[:page], :per_page => 2)
+    @comments = @product.comments.paginate(:page => params[:page], :per_page => 15)
   end
 
   # GET /products/new
